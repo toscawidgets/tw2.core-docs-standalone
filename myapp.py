8881b9920b6e8ef2506511002922410b3c21527c
Ralph Bean <ralph.bean@gmail.com>
2011-05-08 00:52:56 -0400

    Step three -- your first form

--- myapp.py
+++ myapp.py
@@ -7,5 +7,18 @@ class Index(tw2.core.Page):
     def fetch_data(self, req):
         self.req = str(req)
 
+class Movie(tw2.forms.FormPage):
+    title = 'Movie'
+    class child(tw2.forms.TableForm):
+        title = tw2.forms.TextField(validator=tw2.core.Required)
+        director = tw2.forms.TextField()
+        genre = tw2.forms.CheckBoxList(
+            options=['Action', 'Comedy', 'Romance', 'Sci-fi'])
+        class cast(tw2.forms.GridLayout):
+            extra_reps = 5
+            character = tw2.forms.TextField()
+            actor = tw2.forms.TextField()
+
+
 tw2.core.dev_server()
 
