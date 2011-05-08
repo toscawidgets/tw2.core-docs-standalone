fd005655cbca48d8dbe153922919ca5d80ac198c
Ralph Bean <ralph.bean@gmail.com>
2011-05-08 00:52:38 -0400

    Step 2 -- dynamic content.

--- myapp.py
+++ myapp.py
@@ -1,7 +1,11 @@
 import tw2.core
+import tw2.forms
 
 class Index(tw2.core.Page):
     template = 'genshi:./index.html'
+
+    def fetch_data(self, req):
+        self.req = str(req)
 
 tw2.core.dev_server()
 
